46146ec5e80e8688f4cd22d44a20589fa30432f7
laasc <254847941@qq.com>
2022-05-07 17:31:04 +0800

    开发背景图片切换

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -19,7 +19,8 @@
           @focus="setMaskOpen"
           @blur="maskOpen = false"
       />
-      <div class="search_tips" :class="maskOpen?'mask_tips':''">tips: 按 Ctrl + ~ 键自动对焦搜索框，再次按 Ctrl + ~ 键解除搜索框对焦，按 Enter 键自动搜索
+      <div class="search_tips" :class="maskOpen?'mask_tips':''">tips: 按 Ctrl + ~ 键自动对焦搜索框，再次按 Ctrl + ~ 键解除搜索框对焦，按 Enter
+        键自动搜索
       </div>
     </div>
 
@@ -51,21 +52,48 @@
         <a-button @click="development" type="primary" shape="round" icon="picture" size="small"
                   style="margin-left:12px;"/>
       </a-tooltip>
-<!--      <a-tooltip placement="top">-->
-<!--        <template slot="title">-->
-<!--          <span>设为主页</span>-->
-<!--        </template>-->
-<!--        <a-button @click="development" type="primary" shape="round" icon="home" size="small" style="margin-left:12px;"/>-->
-<!--      </a-tooltip>-->
+            <a-tooltip placement="top">
+              <template slot="title">
+                <span>博客主页</span>
+              </template>
+              <a-button @click="goBlog" type="primary" shape="round" icon="home" size="small" style="margin-left:12px;"/>
+            </a-tooltip>
 
     </div>
 
 
-    <iframe v-if="isUserIframe" class="bg_img" width="100%" height="100%" :src=" baseUrl + iframeUrlBase"
+    <iframe v-if="iframeUrlBase.type === 'iframe'" class="bg_img" width="100%" height="100%" :src="iframeUrlBase.src"
             frameborder="0"></iframe>
-    <img v-else class="bg_img" :src="baseUrl + imageUrlBase" alt="bg">
+    <img v-else class="bg_img" :src="imageUrlBase.preview" alt="bg">
 
     <div :class="maskOpen?'mask':'hidden'"></div>
+
+    <a-drawer
+        title="背景图"
+        placement="right"
+        class="drawer_wrap"
+        :closable="false"
+        :width="720"
+        :visible="visible"
+        @close="onClose"
+    >
+      <a-tabs :activeKey="darwsTabsActive" @change="callback">
+        <a-tab-pane :tab="item.name" :key="index" v-for="(item, index) in tabDataList">
+          <div class="tab_pane_box">
+            <div class="draw_img_box" @click="changeBgImg(items, item)" v-for="(items, key) in item.list" :key="key">
+              <a-card-grid style="width:100%;height: 100%;padding: 0;">
+                <div class="draw_bg_img">
+                  <img :src="items.preview" :alt="items.name">
+                </div>
+                <div class="draw_bg_name">{{ items.name }}</div>
+              </a-card-grid>
+            </div>
+
+          </div>
+        </a-tab-pane>
+      </a-tabs>
+    </a-drawer>
+
 
   </div>
 </template>
@@ -74,50 +102,76 @@
 import defaultHome from "@/constant/defaultHome";
 
 import {COVER_HOST} from "../../tool/imgBase";
-import {delLocalStorage, getLocalStorage, setLocalStorage} from "../../utils/stroage";
+import {getLocalStorage, setLocalStorage} from "../../utils/stroage";
+import dataList, {DEFAULT_COVER} from "@/constant/defaultTabPane";
+
+
 export default {
   name: "Home",
   data() {
     return {
       isBaidu: true,
+      darwsTabsActive: 0,
       searchValue: "", //搜索框内容
       maskOpen: false, // mask
       baseUrl: COVER_HOST,
       isUserIframe: true, //是否使用iframe
       linkDataList: defaultHome.keyContentMap.myLike,
       isGhostStyle: false, // 是否选中白色背景背景
-      iframeUrlBase: "/dynamic/cover-01/index.html",
-      imageUrlBase: "/comic/cover_7.jpg"
+      iframeUrlBase: DEFAULT_COVER,
+      imageUrlBase: "/comic/cover_7.jpg",
+      visible: false,
+      tabDataList: dataList
     }
   },
   created() {
     this.handlerFocusSearch()
-    if(getLocalStorage("ghost_style")) {
-      this.isGhostStyle = getLocalStorage("ghost_style") === 1
+    if (getLocalStorage("ghost_style")) {
+      this.isGhostStyle = getLocalStorage("ghost_style") === "currentWhite";
     }
+    if(getLocalStorage("currentImage"))
 
   },
   mounted() {
     this.$nextTick(() => {
-      setLocalStorage("ghost_style", this.isGhostStyle?1:0)
+      setLocalStorage("ghost_style", this.isGhostStyle ? "currentWhite" : "currentGhost")
     })
   },
   methods: {
+    // 跳转博客
+    goBlog() {
+      window.open(`https://myfanfou.top`);
+    },
+    // 点选背景图片
+    changeBgImg(items, item) {
+      console.log(items)
+      this.iframeUrlBase = {...items}
+      setLocalStorage("currentImage", items)
+      this.$forceUpdate();
+    },
+    // 切换抽屉tab栏
+    callback(key) {
+      this.darwsTabsActive = key;
+    },
+    // 关闭抽屉
+    onClose() {
+      this.visible = false;
+    },
     // 切换透明度
     changeSwitch(e) {
       this.isGhostStyle = e;
       // 设置当前机器配置
-      setLocalStorage("ghost_style", e?1:0)
+      setLocalStorage("ghost_style", e ? "currentWhite" : "currentGhost")
     },
     // 按ctrl focus聚焦搜索框
     handlerFocusSearch() {
       let that = this;
       // eslint-disable-next-line no-unused-vars
       let keyUp = false;
-      if(keyUp) return;
+      if (keyUp) return;
       document.onkeydown = function (event) {
         let e = event || window.event;
-        if ((e.ctrlKey)&&(e.keyCode === 192)) {
+        if ((e.ctrlKey) && (e.keyCode === 192)) {
           keyUp = true;
           setTimeout(() => {
             keyUp = false;
@@ -144,7 +198,7 @@ export default {
     },
 
     development() {
-      alert("别急，还在开发中")
+      this.visible = true;
     },
 
     // 搜索
@@ -170,5 +224,43 @@ export default {
 <style scoped lang="scss">
 @import './Home.scss';
 @import './ghost.css';
+
+.drawer_wrap {
+  .tab_pane_box {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+
+    .draw_img_box {
+      padding: 0 10px;
+      width: 25%;
+      min-height: 150px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-bottom: 20px;
+      cursor: pointer;
+
+      .draw_bg_img {
+        width: 100%;
+        height: 150px;
+
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+      }
+
+      .draw_bg_name {
+        text-align: center;
+        margin: 5px 0;
+      }
+    }
+  }
+
+}
 
 </style>
